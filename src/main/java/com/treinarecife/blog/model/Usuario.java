379398003eb941ec6a9ef9d44f0ea05a993300c7
viniciusGli <vinicius.gutemberg1@gmail.com>
2023-09-09 14:05:46 -0300

    *Algumas modificações feitas pela a IDE* Criação do UsuarioController

--- src/main/java/com/treinarecife/blog/model/Usuario.java
+++ src/main/java/com/treinarecife/blog/model/Usuario.java
@@ -1,9 +1,12 @@
 package com.treinarecife.blog.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,12 +21,18 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idUsuario;
 
+    @Column(nullable = false, length = 50)
     private String nome;
 
+    @Column(nullable = false, length = 150)
     private String sobrenome;
 
+    @NotEmpty(message = "Email cannot be empty")
+    @Column(nullable = false, length = 100, unique = true)
     private String email;
 
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,16}$")
+    @Column(nullable = false, length = 50)
     private String senha;
-    
+
 }
---
+++ src/main/java/com/treinarecife/blog/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.treinarecife.blog.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.treinarecife.blog.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+
+    
+} 
+
+    
