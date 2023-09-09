13c8e87f7a3f1ff93107e7d1b11cc3e6c253a289
viniciusGli <vinicius.gutemberg1@gmail.com>
2023-09-09 14:52:09 -0300

    Criação do Comentario  model

---
+++ src/main/java/com/treinarecife/blog/model/Comentario.java
@@ -0,0 +1,33 @@
+package com.treinarecife.blog.model;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "tb_comentarios")
+public class Comentario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idComentario;
+
+    @Column(nullable=false)
+    private String texto;
+
+    @Column()
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataDeCriacao;
+
+}
