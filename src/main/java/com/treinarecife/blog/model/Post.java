c928fbb74cbc2ecf7f1437dae74aace30c216655
viniciusGli <vinicius.gutemberg1@gmail.com>
2023-09-09 14:12:31 -0300

    Criação da entidade Post

---
+++ src/main/java/com/treinarecife/blog/model/Post.java
@@ -0,0 +1,36 @@
+package com.treinarecife.blog.model;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "tb_posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPost;
+
+    @Column(nullable = false, length = 255)
+    private String titulo;
+
+    @Column(length = 100)
+    private String conteudo;
+
+    @Column(nullable = false)
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataDeCriacao;
+    
+}
