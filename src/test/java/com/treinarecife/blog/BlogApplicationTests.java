3cdfc2a2c3ae361d824d2dd911fcb0265815960a
viniciusGli <vinicius.gutemberg1@gmail.com>
2023-09-09 13:22:36 -0300

    Criação da aplicacção 'blog'

---
+++ src/test/java/com/treinarecife/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treinarecife.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
